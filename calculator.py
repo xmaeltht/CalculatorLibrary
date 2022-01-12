5efa60c98cf2b97137cf75c6369907b4f4751fdc
mael <xmaeltht@gmail.com>
2022-01-12 00:03:50 -0500

    add unit test for calculator and requirements file

--- calculator.py
+++ calculator.py
@@ -1,9 +1,11 @@
-# Calculator Library containing basic match operations.
+"""
+Calculator library containing basic math operations.
+"""
 
 
 def add(first_term, second_term):
     return first_term + second_term
 
 
-def substract(first_term, second_term):
+def subtract(first_term, second_term):
     return first_term - second_term
